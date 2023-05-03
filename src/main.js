d760a3d69f680c3cec1c0c04d32f98f6cdb97ba0
axgs <90789828+axgs@users.noreply.github.com>
2023-05-03 22:09:19 +0200

    changes

--- src/main.js
+++ src/main.js
@@ -1,19 +1,17 @@
+// library files
 import {Agl} from "./js/Agl.js";
-import {Player} from "./js/Sprites/Player.js";
 import {Tilemap} from "./js/Tilemap.js";
+
+// game objects
+import {Player} from "./js/Sprites/Player.js";
 
 // tilemap data
 import {tileMapDataLevel1} from "./js/tilemaps/level1.js";
 
-
-// game size
-const gameWidth = 320;
-const gameHeight = 256;
-
 // game objects
 let player;
 
-// game background
+// game background, fonts etc.
 let tilemap;
 let font6x7;
 
@@ -29,11 +27,10 @@ const images = [
  */
 const sounds = [{id: "snd1", file: "snd1.ogg"}];
 
-/**
- * init game and preload all data
- */
-Agl.init("screen", gameWidth, gameHeight);
+Agl.init("screen", 320, 256);
+
 Agl.addPreloadImages(images);
+
 Agl.preloadImages(() => {
     tilemap = new Tilemap(tileMapDataLevel1, "tiles1");
     font6x7 = Agl.getImage("font6x7");
